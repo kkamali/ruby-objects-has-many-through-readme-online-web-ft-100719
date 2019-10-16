909b3fdd4eed7bb6e7e38024ce9847b062cdabf1
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-16 21:28:33 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -2,4 +2,9 @@ class Meal
   attr_accessor :waiter, :customer, :total, :tip 
   
   def intialize(waiter, customer, total, tip)
+    @waiter = waiter 
+    @customer = customer 
+    @total = total 
+    @tip = tip 
+  end
 end
