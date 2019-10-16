ce5369c02cd3304b9e260181ca82c2a19577d7d3
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-16 21:29:43 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -3,7 +3,7 @@ class Meal
   
   @@all = []
   
-  def intialize(waiter, customer, total, tip=-)
+  def intialize(waiter, customer, total, tip=0)
     @waiter = waiter 
     @customer = customer 
     @total = total 
